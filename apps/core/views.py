3bc094a864d4c7ce49d74349d2822f313829e846
Sina <mohamadaminifar81@gmail.com>
2026-02-11 15:38:49 +0330

    Template Inheritance

--- apps/core/views.py
+++ apps/core/views.py
@@ -13,7 +13,7 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = "core/home.html"
+    template_name = "core/templates/home.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
