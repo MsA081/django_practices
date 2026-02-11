b67041e7486c1cd85f22baec22c3770a6d1dac53
Sina <mohamadaminifar81@gmail.com>
2026-02-11 15:32:23 +0330

    The Interface CBV

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# فعلاً از FBV (Function Based View) استفاده می‌کنیم تا جریان را درک کنیم
-def home(request):
-    # 1. Logic: (مثلاً گرفتن آخرین اخبار)
-    context = {
-        'title': 'هومینو | خدمات آنلاین منزل',
-        'user': request.user
-    }
-    # 2. Response: تحویل به تمپلیت
-    return render(request, 'core/home.html', context)
+# # فعلاً از FBV (Function Based View) استفاده می‌کنیم تا جریان را درک کنیم
+# def home(request):
+#     # 1. Logic: (مثلاً گرفتن آخرین اخبار)
+#     context = {
+#         'title': 'هومینو | خدمات آنلاین منزل',
+#         'user': request.user
+#     }
+#     # 2. Response: تحویل به تمپلیت
+#     return render(request, 'core/home.html', context)
+
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "core/home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "هومینو | صفحه اصلی"
+        return context
